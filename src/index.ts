8c2516c39ad24ae0ca403fb0684aceb6016c8ce9
Oleg Arbuzov <oleg.arbuzov@rt.ru>
2024-11-08 16:58:51 +0300

    refactoring: выносит вызов getData в index

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-import './get-data.js'
+import { getData } from "./get-data.js";
+import { UserData } from "./constants.js";
+
+const { COMMENTS_URL } = UserData;
+
+getData<UserData.User[]>(COMMENTS_URL)
+  .then((data) => {
+    data.forEach((el: UserData.User) => {
+        console.log(`ID: ${el.id}, Email: ${el.email} `);
+    });
+  });
